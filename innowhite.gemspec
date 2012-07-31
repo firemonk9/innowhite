841435db4ee2c6a3caf9522f1f7d407a3e55e159
Jenua Boiko <jeyboy@bigmir.net>
2012-07-31 19:00:41 +0300

    fin

--- innowhite.gemspec
+++ innowhite.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{innowhite}
-  s.version = "1.1.9"
+  s.version = "2.0.3"
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2") if s.respond_to? :required_rubygems_version=
   s.authors = [%q{innowhite}]
   s.date = %q{2011-08-11}
@@ -16,7 +16,6 @@ Gem::Specification.new do |s|
   s.rubyforge_project = %q{innowhite_api}
   s.rubygems_version = %q{1.8.6}
   s.summary = %q{Gem for Innowhite Api}
-  s.add_dependency 'nokogiri', '~> 1.4.4'
   s.add_dependency 'rest-client', '~> 1.6.7'
   s.add_development_dependency "rspec", "~> 1.3.4"
 
--- lib/innowhite.rb
+++ lib/innowhite.rb
@@ -1,206 +1,99 @@
-require 'open-uri'
-require 'nokogiri'
 require 'rest-client'
 
 class Innowhite
-
-  @@errors = {
-    :user_miss =>         [1, "user value missed"],
-    :fetch_fail =>        [2, "failed to fetch, maybe you have entered wrong username"],
-    :room_miss =>         [3, "room not set"],
-    :description_miss =>  [4, "description value not set"],
-    :start_time =>        [5, "startTime value missed"],
-    :end_time =>          [6, "endTime value missed"],
-    :time_zone =>         [7, "timeZone value missed"],
-  }
-
   attr_accessor :mod_name, :org_name, :sub, :server_address, :private_key
 
   def initialize
     load_settings
   end
 
-  def create_room(params = {})
-    return err(*@@errors[:user_miss]) if params[:user].blank?
-    room_id = get_room_id(params[:user])
-    return room_id[:errors] if room_id.has_key?(:errors)
-    res = create_room_info(room_id[:data], params[:user], params[:tags], params[:desc], @org_name)
-    res[:status] ? data({"room_id" => room_id[:data], "address" => res[:data], "credentials" => res[:credentials]}) : res[:errors]
+  def create_room(fullName, meetingName, params = {})
+    params.reverse_merge! :duration => 0, :meetingName => meetingName
+    params.slice!( :meetingID, :attendeePW,
+                  :moderatorPW, :welcome,
+                  :dialNumber, :voiceBridge,
+                  :webVoice, :record, :meta,
+                  :duration, :meetingName )
+    checker([:meetingName], params)
+    request(:post, "api_create_room", {:data => params, :fullName => fullName})
   end
 
-  def join_meeting(room_id, user)
-    return err(*@@errors[:user_miss]) if user.nil?
-    return err(*@@errors[:room_miss]) if room_id.nil?
-    url = "#{@api_address}exist_session?roomId=#{room_id}"
-    res = request("post", url)
-    return res[:errors] if res.has_key?(:errors)
-    res[:data].blank? ? err(-1, "Unknow") : data(join_room_url(@org_name, room_id, user, false))
+  def join_meeting(fullName, meetingID, password)
+    params = {:meetingID => meetingID, :password => password, :fullName => fullName }
+    checker([:meetingID, :password, :fullName], params)
+    request(:post, "api_join_room", :data => params)
   end
+
 
   def get_sessions(params = {})
-    temp = url_generator(params[:parentOrg] || @parent_org, params[:orgName] || @org_name)
-    checksum = generating_checksum(URI.escape(temp))
-    tmp = "#{temp}&user=#{params[:user]}&tags=#{params[:tags]}"
-    url = URI.escape("#{@api_address}list_sessions?#{tmp}&checksum=#{checksum}")
-    request("get", url)
-
-  rescue
-    err(*@@errors[:fetch_fail])
+    params.slice!( :fullName, :tags, :organizationName )
+    request(:get, "list_sessions", params)
   end
 
   def schedule_meeting(params = {})
-    return err(*@@errors[:user_miss]) if !params[:user] || params[:user].blank?
-    return err(*@@errors[:description_miss]) if !params[:description] || params[:description].empty?
-
-    return err(*@@errors[:start_time]) if !params[:startTime] || params[:startTime].blank?
-    return err(*@@errors[:end_time]) if !params[:endTime] || params[:endTime].blank?
-    return err(*@@errors[:time_zone]) if !params[:timeZone] || params[:timeZone].blank?
-
-    room_id = get_room_id(params[:user])
-    return room_id[:errors] if room_id.has_key?(:errors)
-
-    create_schedule(
-        room_id[:data],
-        params[:user],
-        params[:tags],
-        params[:description],
-        params[:parentOrg] || @parent_org,
-        params[:startTime],
-        params[:endTime],
-        params[:timeZone])
+    params.slice!( :fullName, :description, :startTime, :endTime, :timeZone, :tags )
+    checker([:fullName, :description, :startTime, :endTime, :timeZone], params)
+    request(:post, "create_schedule_meeting", params)
   end
 
   def past_sessions(params = {})
-    temp = url_generator(params[:parentOrg] || @parent_org, params[:orgName] || @org_name)
-    checksum = generating_checksum(URI.escape(temp))
-
-    tmp = "#{temp}&user=#{params[:user]}&tags=#{params[:tags]}"
-    url = URI.escape("#{@api_address}past_sessions?#{tmp}&checksum=#{checksum}")
-    request("get", url)
-
-    rescue
-      err(*@@errors[:fetch_fail])
+    params.slice!( :fullName, :tags, :organizationName )
+    request(:get, "past_sessions", params)
   end
 
   def get_scheduled_list(params={})
-    checksum = main_cheksum(params[:parentOrg] || @parent_org, params[:orgName] || @org_name)
-    par = url_generator(params[:parentOrg] || @parent_org, params[:orgName] || @org_name)
-    url = URI.escape("#{@api_address}get_scheduled_sessions?#{par}&checksum=#{checksum}&tags=#{params[:tags]}&user=#{params[:user]}")
-    request("get", url)
+    params.slice!( :fullName, :tags )
+    request(:get, "get_scheduled_sessions", params)
   end
 
-  def cancel_meeting(room_id)
-    checksum = main_cheksum(@parent_org, @org_name)
-    par = url_generator(@parent_org, @org_name)
-    url = URI.escape("#{@api_address}cancel_meeting?roomId=#{room_id}&#{par}&checksum=#{checksum}")
-    data(request("get", url))[:status]
+  def cancel_meeting(meetingID, password)
+    checker([:meetingID], (hash = {:meetingID => meetingID, :password => password}))
+    request(:post, "api_join_room", hash)
   end
 
-  def update_schedule(params = {})
-    checksum = main_cheksum(@parent_org, @org_name)
-    params[:startTime] = params[:startTime].to_i  if !params[:startTime].blank? && (params[:startTime].is_a?(DateTime) || params[:startTime].is_a?(Time))
-    params[:endTime] = params[:endTime].to_i  if !params[:endTime].blank? && (params[:endTime].is_a?(DateTime) || params[:endTime].is_a?(Time))
-
-    request("put", "#{@api_address}update_schedule",
-                             {:roomId => params[:room_id], :tags => params[:tags], :description => params[:description],
-                              :parentOrg => @parent_org, :orgName => @org_name,
-                              :checksum => checksum, :startTime => params[:startTime],
-                              :endTime => params[:endTime], :timeZone => params[:timeZone]
-                             }
-        )[:status]
+  def update_schedule(meetingID, params = {})
+    return true if params.empty?
+    params.update(:meetingID => meetingID)
+    params.slice!( :meetingID, :startTime, :endTime, :timeZone, :description, :tags )
+    checker([:meeting_id], params)
+    request(:put, "update_schedule", params)
   end
 
-  def getRecordingURL(room_id)
-    url = "#{@api_address}api_get_recording_urls?#{{:room_id => room_id}.to_param}"
-    request("get", url)
-  end
-
-  def self.gen_checksum(params)
-    key = params.delete(:key)
-    temp = params.to_param
-    temp + "&checksum=#{Digest::SHA1.hexdigest(URI.escape(temp) + key)}"
-  end
-
-  def get_room_id(user)
-    url = "#{@api_address}api_create_room?#{{:parentOrg => @parent_org, :orgName => @org_name, :user => user, :key => @private_key}.to_param}"
-    request("get", url)
+  def get_recordings(meetingID)
+    checker([:meetingID], (hash = {:meetingID => meetingID}))
+    request(:get, "api_get_recording_urls", hash)
   end
 
   protected
-    def err(code, message)
-      {"errors" => {"message" => message, "code" => code}}.with_indifferent_access
-    end
-
-    def data(values)
-      {"data" => values}.with_indifferent_access
-    end
-
     def load_settings
-      settings = YAML.load_file('config/innowhite.yml')# if RAILS_ENV == "development"
-      @api_address = settings["innowhite"]["api_address"]
-      @private_key = settings["innowhite"]["private_key"]
-      @parent_org = settings["innowhite"]["organization"]
-      @org_name = @parent_org
-    end
-
-    def create_schedule(room_id, user, tags, desc, parent_org, start_time, end_time, time_zone)
-      checksum = generating_checksum(URI.escape(url_generator(parent_org, parent_org)))
-      request("post", "#{@api_address}create_schedule_meeting",
-                            {:roomId => room_id, :user => user, :tags => tags, :desc => desc, :startTime => start_time,
-                             :endTime => end_time, :timeZone => time_zone,
-                             :parentOrg => parent_org, :orgName => parent_org,
-                             :checksum => checksum,  :roomLeader => true,
-                             :key => @private_key
-                            }
-      )
-    end
-
-    def create_room_info(room_id, user, tags, desc, parent_org)
-      checksum = generating_checksum(URI.escape(url_generator(parent_org, parent_org)))
-
-      request("post", "#{@api_address}create_room_info",
-        {:roomId => room_id, :user => user, :tags => tags, :desc => desc,
-         :parentOrg => parent_org, :orgName => parent_org,
-         :checksum => checksum, :key => @private_key
-        }
-      )
+      settings = YAML.load_file('config/innowhite.yml')
+      @api_address = settings["api_address"]
+      @private_key = settings["private_key"]
+      @parent_org = settings["organization"]
     end
 
   private
+    def checker(fields, hash)
+      fields.each do |field|
+        (raise exception([field.humanize, "not set"].join(" ")) and return) if hash[field].blank?
+      end
+    end
 
-    def request(method, url, *params)
-      url = url + "?" + params.to_param unless params.blank?
-      k = if method == "get" || method == "delete"
+    def prepare_url(url, params)
+      params.update( :orgName => params[:parentOrg] || @parent_org )
+      params.update :checksum => Digest::SHA1.hexdigest(params.to_param) # + @private_key
+      [url, "?", params.to_param].join
+    end
+
+    def request(method, url, params = {})
+      url = prepare_url(@api_address + url, params)
+      k = if [:get, "get", :delete, "delete"].include?(method)
         RestClient.send(method, url, :accept => :json)
       else
         RestClient.send(method, url, {}, :accept => :json)
       end
-      JSON::parse(k).with_indifferent_access
-    end
-
-    def main_cheksum(parent_org, org_name)
-      checksum_tmp = url_generator(parent_org, org_name)
-      generating_checksum(URI.escape(checksum_tmp))
-    end
-
-    def generating_checksum(params)
-      Digest::SHA1.hexdigest(params + @private_key)
-    end
-
-    def generate_checksum(parent_org, org_name, user_name)
-      Digest::SHA1.hexdigest(information_url(parent_org, org_name, user_name))
-    end
-
-    def url_generator(parent_org, org_name)
-      "parentOrg=#{parent_org}&orgName=#{org_name}"
-    end
-
-    def join_room_url(org_name, room_id, user, is_teacher)
-      url = "#{@api_address}api_join_room?#{{:parentOrg => @parent_org, :orgName => org_name, :user => user, :roomId => room_id, :roomLeader => is_teacher, :key => @private_key}.to_param}"
-      request("post", url)[:data]
-    end
-
-    def information_url(parent_org, org_name, user_name)
-      "parentOrg=#{parent_org}&orgName=#{org_name}&user=#{user_name}#{@private_key}"
+      res = JSON::parse(k).with_indifferent_access
+      raise exception(res[:errors]) if res.has_key?(:errors)
+      res.try(:[], :status) || res
     end
 end
